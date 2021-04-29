c55dfa9fb958d452d465b3f32acb75ccba9bcecf
Grigorii Korshunov <g.korshunov@axitech.ru>
2021-04-29 16:33:56 +0300

    first init

---
+++ example/askug_dev_config_model_example.dart
@@ -0,0 +1,6 @@
+import 'package:askug_dev_config_model/askug_dev_config_model.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/askug_dev_config_model_test.dart
@@ -0,0 +1,16 @@
+import 'package:askug_dev_config_model/askug_dev_config_model.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
